b87b1fb7cd0bbb2fa53ac5113bfc6d9ef5f75d30
cerkhachacu <yanralapdy.xxx@gmail.com>
2020-10-12 20:28:41 +0700

    update give option to chose which function to use

--- excercise/excercise1.go
+++ excercise/excercise1.go
@@ -11,13 +11,30 @@ import (
 
 func main() {
 	// request input number
-	reader := bufio.NewScanner(os.Stdin)
+	reader1 := bufio.NewScanner(os.Stdin)
 	fmt.Print("Enter odd number bigger than 1 : ")
-	scanner := reader.Scan() // scan input
-	if scanner {             //if input exist
-		number, err := strconv.ParseInt(reader.Text(), 10, 32) // conver input into integer
-		if err == nil {                                        // success convert to integer
-			patternH1(number) // calling function to create the pattern
+	scanner1 := reader1.Scan() // scan input
+	reader2 := bufio.NewScanner(os.Stdin)
+	fmt.Print("Chose method 1 or 2 : ")
+	scanner2 := reader2.Scan() // scan input
+	if scanner1 {              //if input exist
+		number, err := strconv.ParseInt(reader1.Text(), 10, 32) // conver input into integer
+		if err == nil {
+			if scanner2 {
+				methodChosen, err := strconv.ParseInt(reader2.Text(), 10, 32)
+				if err == nil {
+					switch methodChosen {
+					case 1:
+						patternH1(number) // calling function to create the pattern
+					case 2:
+						patternH2(number)
+					default:
+						fmt.Println("Please chose between method 1 or 2.")
+					}
+				} else {
+					fmt.Println("Invalid input, only number is accepted")
+				}
+			} // success convert to integer
 		} else { // if the input is not a number
 			fmt.Println("Invalid input, only number is accepted")
 		}
