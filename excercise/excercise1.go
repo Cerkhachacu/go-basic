98307cbb3274541077c973745e6771796f87a360
cerkhachacu <yanralapdy.xxx@gmail.com>
2020-10-12 13:48:17 +0700

    update function create pattern

--- excercise/excercise1.go
+++ excercise/excercise1.go
@@ -6,6 +6,7 @@ import (
 	"math"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -16,37 +17,65 @@ func main() {
 	if scanner {             //if input exist
 		number, err := strconv.ParseInt(reader.Text(), 10, 32) // conver input into integer
 		if err == nil {                                        // success convert to integer
-			if number%2 != 0 && number > 1 { // cek if input is odd number
-				var numberCu int64 = number*number + 1
-				var i int64 = 1
-				mid := math.Round(float64(number) / 2)
-				var r float64 = 1
-				firstCol := true
-				for i < numberCu { // creating the input
-					if i%number != 0 {
-						if firstCol {
-							fmt.Print("* ")
-							firstCol = false
-						} else {
-							if r == mid {
-								fmt.Print("* ")
-								firstCol = false
-							} else {
-								fmt.Print("= ")
-							}
-						}
-					} else {
-						r++
-						fmt.Print("*\n")
-						firstCol = true
-					}
-					i++
-				}
-			} else { // if the input is not an odd number print error
-				fmt.Println("Please input odd number that is bigger than one")
-			}
+			patternH1(number) // calling function to create the pattern
 		} else { // if the input is not a number
 			fmt.Println("Invalid input, only number is accepted")
 		}
 	}
+}
+
+func patternH1(number int64) { // success convert to integer
+	if number%2 != 0 && number > 1 { // cek if input is odd number
+		var numberCu int64 = number*number + 1
+		var i int64 = 1
+		mid := math.Round(float64(number) / 2)
+		var r float64 = 1
+		firstCol := true
+		for i < numberCu { // as long as
+			if i%number != 0 { // if i modulus number equal 0
+				if firstCol {
+					fmt.Print("* ")
+					firstCol = false
+				} else {
+					if r == mid {
+						fmt.Print("* ")
+						firstCol = false
+					} else {
+						fmt.Print("= ")
+					}
+				}
+			} else {
+				r++
+				fmt.Print("*\n")
+				firstCol = true
+			}
+			i++
+		}
+	} else { // if the input is not an odd number print error
+		fmt.Println("Please input odd number that is bigger than one")
+	}
+}
+
+func patternH2(number int64) { // success convert to integer
+	if number%2 != 0 && number > 1 { // cek if input is odd number
+		var i float64 = 1
+		mid := math.Ceil(float64(number) / 2)
+		for i <= float64(number) {
+			if mid != i { // creating "* = = = *" part
+				starSign := "* *\n"
+				equalSign := strings.Repeat(" =", int(number-2))
+				output := starSign[:1] + equalSign + starSign[1:]
+				fmt.Print(output)
+			} else { // creating "* * * * *"
+				starSign := "* *\n"
+				equalSign := strings.Repeat(" *", int(number-2))
+				output := starSign[:1] + equalSign + starSign[1:]
+				fmt.Print(output)
+			}
+			i++
+		}
+	} else { // if the input is not an odd number print error
+		fmt.Println("Please input odd number that is bigger than one")
+	}
+
 }
